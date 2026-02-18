3c93b1aa9f00617bcc7fbdec81065a152bf346e5
Dev.harsh <97585709+harshdeveloper78@users.noreply.github.com>
2026-02-18 16:16:35 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,22 @@
+// block scopped variable or only used in block
+//value can be reassigned 
+let score = 50;
+console.log(score);
+
+score = 55;
+console.log(score);
+
+//value can not be reassigned
+const name1 = 'harsh';
+const age1 = 23;
+console.log(name1 , age1);
+
+
+//var is globle variable
+// value can be reassigned
+var name2 = 'ashu';
+var age2 = '22';
+console.log(name2, age2);
+
+name2 = 'honey';
+console.log(name2);
