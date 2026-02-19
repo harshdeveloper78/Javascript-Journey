a6bd8d851bb7dd4dd95a6e4123cebca5e0845693
Dev.harsh <97585709+harshdeveloper78@users.noreply.github.com>
2026-02-19 23:16:45 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,53 @@
+//data stored in variable
+
+//primitive datatypes
+//these are immtable
+
+//String
+let name1 = 'harsh';
+console.log(name1);
+
+//numbers
+let age = 23;
+console.log(age);
+
+//boolean
+let datatypes= true;
+console.log(datatypes);
+
+//null
+let data = null;
+console.log(data);
+
+//undefined
+let address;
+console.log(address);
+
+//symbol
+const uniqueId = Symbol('id');
+console.log(uniqueId);
+
+
+//Non primitive datatypes
+//these are mutable
+
+//Objects 
+const person = {
+    name2:'Ashu',
+    age: 22
+};
+console.log(person);
+
+//Array
+let persons = ['Harsh', 'ashu', 'Honey']
+console.log(persons); 
+
+
+//functions
+function add(x, y){
+    return x+y;
+}
+let result = add(3, 4)
+console.log(result);
+
+
